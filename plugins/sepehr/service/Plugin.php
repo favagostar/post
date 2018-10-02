70a38ec3ab746bab5fd64d1c3a16e1b75fb41ae0
ehsan <birnaghdejavan@gmail.com>
2018-10-02 18:07:27 +0330

    update Plugin.php

--- plugins/sepehr/service/Plugin.php
+++ plugins/sepehr/service/Plugin.php
@@ -4,6 +4,20 @@ use System\Classes\PluginBase;
 
 class Plugin extends PluginBase
 {
+
+    public function pluginDetails()
+    {
+        return [
+            "name"          => 'sepehr.service::lang.plugin.name',
+            "description"   => 'sepehr.service::lang.plugin.description',
+            "author"        => "sepehr",
+            "icon"          => "oc-icon-database",
+            "homepage"      => ''
+        ];
+
+
+    }
+
     public function registerComponents()
     {
     }
