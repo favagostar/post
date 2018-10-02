fe4ac44af454ab2873f3f5e00e275c89832f70bc
ehsan <birnaghdejavan@gmail.com>
2018-10-02 18:24:32 +0330

    update Plugin.php

--- plugins/sepehr/service/Plugin.php
+++ plugins/sepehr/service/Plugin.php
@@ -1,5 +1,6 @@
 <?php namespace Sepehr\Service;
 
+use Backend;
 use System\Classes\PluginBase;
 
 class Plugin extends PluginBase
@@ -8,14 +9,40 @@ class Plugin extends PluginBase
     public function pluginDetails()
     {
         return [
-            "name"          => 'sepehr.service::lang.plugin.name',
-            "description"   => 'sepehr.service::lang.plugin.description',
-            "author"        => "sepehr",
-            "icon"          => "oc-icon-database",
-            "homepage"      => ''
+            "name" => 'sepehr.service::lang.plugin.name',
+            "description" => 'sepehr.service::lang.plugin.description',
+            "author" => "sepehr",
+            "icon" => "oc-icon-database",
+            "homepage" => ''
         ];
+    }
 
 
+    public function registerPermissions()
+    {
+        return [
+            "sepehr.service.*" => [
+                "tab" => 'مشخصات سرویس',
+                "label" => ' مدیریت سرویس ها ',
+                "order" => 1
+            ]
+        ];
+    }
+
+    public function registerNavigation()
+    {
+        return [
+            'services' => [
+                'label' => 'سرویس',
+                'url' => Backend::url('sepehr/service/services'),
+                'iconSvg' => 'plugins/sepehr/service/assets/images/service.svg',
+                'permissions' => ['sepehr.service.*'],
+                'order' => 300,
+                'sideMenu' => [
+                    ''
+                ]
+            ]
+        ];
     }
 
     public function registerComponents()
