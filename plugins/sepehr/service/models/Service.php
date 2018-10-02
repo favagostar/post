c83a4f071dafd4d60232488132208444eb978aa1
ehsan <birnaghdejavan@gmail.com>
2018-10-02 18:06:56 +0330

    update Service Model

--- plugins/sepehr/service/models/Service.php
+++ plugins/sepehr/service/models/Service.php
@@ -1,6 +1,15 @@
 <?php namespace Sepehr\Service\Models;
 
+use Backend\Models\User as BackendUser;
+use RainLab\User\Models\User as FrontUser;
+
 use Model;
+use Sepehr\Details\Models\DistributionTime;
+use Sepehr\Details\Models\InsuranceType;
+use Sepehr\Details\Models\PackageType;
+use Sepehr\Details\Models\PaymentType;
+use Sepehr\Details\Models\PostType;
+use Sepehr\Details\Models\SpecialService;
 
 /**
  * Model
@@ -8,7 +17,7 @@ use Model;
 class Service extends Model
 {
     use \October\Rain\Database\Traits\Validation;
-    
+
     use \October\Rain\Database\Traits\SoftDelete;
 
     protected $dates = ['deleted_at'];
@@ -19,8 +28,87 @@ class Service extends Model
     public $rules = [
     ];
 
+
+    public function getUserIdOptions()
+    {
+        $lists = FrontUser::lists('first_name', 'id');
+        $list = [' ' => 'انتخاب کنید'] + $lists;
+        return $list;
+    }
+
+    public function getManagerIdOptions()
+    {
+        $lists = BackendUser::lists('first_name', 'id');
+        $list = [' ' => 'انتخاب کنید'] + $lists;
+        return $list;
+    }
+
+    public function getOperatorIdOptions()
+    {
+        $lists = BackendUser::lists('first_name', 'id');
+        $list = [' ' => 'انتخاب کنید'] + $lists;
+        return $list;
+    }
+
+    public function getPaymentTypeIdOptions()
+    {
+        $lists = PaymentType::lists('name', 'id');
+        $list = [' ' => 'انتخاب کنید'] + $lists;
+        return $list;
+    }
+
+    public function getDistributionTimeIdOptions()
+    {
+        $lists = DistributionTime::lists('name', 'id');
+        $list = [' ' => 'انتخاب کنید'] + $lists;
+        return $list;
+    }
+
+    public function getPostTypeIdOptions()
+    {
+        $lists = PostType::lists('name', 'id');
+        $list = [' ' => 'انتخاب کنید'] + $lists;
+        return $list;
+
+    }
+
+    public function getSpecialServicesIdOptions()
+    {
+        $lists = SpecialService::lists('name', 'id');
+        $list = [' ' => 'انتخاب کنید'] + $lists;
+        return $list;
+
+    }
+
+    public function getPackageTypeIdOptions()
+    {
+        $lists = PackageType::lists('name', 'id');
+        $list = [' ' => 'انتخاب کنید'] + $lists;
+        return $list;
+
+    }
+
+    public function getInsuranceTypeIdOptions()
+    {
+        $lists = InsuranceType::lists('name', 'id');
+        $list = [' ' => 'انتخاب کنید'] + $lists;
+        return $list;
+
+    }
+
+    public function getPostmanIdOptions()
+    {
+        $lists = FrontUser::lists('first_name', 'id');
+        $list = [' ' => 'انتخاب کنید'] + $lists;
+        return $list;
+
+    }
+
+
     /**
      * @var string The database table used by the model.
      */
     public $table = 'sepehr_service_index';
+
+
 }
