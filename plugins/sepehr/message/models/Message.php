d5177275190c6b65d054edc738bf19db5985913f
ehsan <birnaghdejavan@gmail.com>
2018-10-07 12:54:03 +0330

    update Message Model

--- plugins/sepehr/message/models/Message.php
+++ plugins/sepehr/message/models/Message.php
@@ -9,7 +9,7 @@ use RainLab\User\Models\User;
 class Message extends Model
 {
     use \October\Rain\Database\Traits\Validation;
-    
+
     use \October\Rain\Database\Traits\SoftDelete;
 
     protected $dates = ['deleted_at'];
@@ -18,7 +18,24 @@ class Message extends Model
      * @var array Validation rules
      */
     public $rules = [
+        'users' => 'required',
+        'description' => 'required',
+
     ];
+    public $customMessages = [
+        'description.required' => 'لطفا متن پیام را وارد نمایید',
+        'users.required' => 'لطفا کاربر را انتخاب نمایید',
+    ];
+
+    public function beforeValidate()
+    {
+        foreach ($this->users as $key => $value) {
+            $this->rules['users.' . $key . '.user_id'] = 'required';
+
+            $this->customMessages['users.' . $key . '.user_id.required'] = 'لطفا کاربر را انتخاب نمایید';
+
+        }
+    }
 
     /**
      * @var string The database table used by the model.
@@ -28,18 +45,17 @@ class Message extends Model
     public function getUserIdOptions()
     {
         $users = User::whereIsActivated(1)
-                       ->orderBy('last_name')
-                       ->orderBy('first_name')
-                       ->get();
+            ->orderBy('last_name')
+            ->orderBy('first_name')
+            ->get();
 
-        $list = [' ' =>'انتخاب کنید'];
-        if($users->count())
-        {            
+        $list = [' ' => 'انتخاب کنید'];
+        if ($users->count()) {
             foreach ($users as $item) {
-                $list[$item->id] = "$item->first_name ، $item->last_name" . 
-                                    ($item->national_code ? ' - '.$item->national_code : '');
-            }       
+                $list[$item->id] = "$item->first_name ، $item->last_name" .
+                    ($item->national_code ? ' - ' . $item->national_code : '');
+            }
         }
         return $list;
-    }    
+    }
 }
